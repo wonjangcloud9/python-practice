a2f24d46da7c124ef7b8de2100156f00679e8d08
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-12-19 21:19:57 +0900

    math

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-def say_hello(name="anonymous"):
-    print(f"{name} It's me")
+def plus(a=0, b=0):
+    print(a + b)
 
 
-say_hello()
+def minus(a=0, b=0):
+    print(a - b)
+
+
+def multiplication(a=0, b=0):
+    print(a * b)
+
+
+def division(a=1, b=1):
+    print(a / b)
+
+
+plus()
+plus(5, 5)
+
+minus()
+minus(5, 5)
+
+multiplication()
+multiplication(5, 5)
+
+division()
+division(5, 5)
