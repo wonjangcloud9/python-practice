6724f66b13becccb06edab21a97e4562c575bba7
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-12-20 20:40:09 +0900

    else elif

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-if 10 > 5:
-    print("Correct!")
+winner = 10
+
+if winner > 10:
+    print("Winner is greater than 10")
+elif winner < 10:
+    print("Winner is less than 10")
+else:
+    print("Winner is 10")
